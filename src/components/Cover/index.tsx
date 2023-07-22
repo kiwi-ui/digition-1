972dd0042727f3a6d2cc3fa047c2293b7966bec8
kiwi-ui <yusnianggara@gmail.com>
2023-07-22 20:04:35 +0700

    setup build

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import profileImage from '../../assets/img/g-1.webp';
 import './style.css';
-import { BsEnvelopeHeart } from 'react-icons/bs'
+import { BsEnvelopeHeart } from 'react-icons/bs';
 
 const Cover = ({hide, setHide}: any) => {
     const { name }: any = useParams();
