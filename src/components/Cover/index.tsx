ec2f17523127e44ff7c2f9efbcf13ec852d19a37
kiwi-ui <yusnianggara@gmail.com>
2023-07-23 10:10:42 +0700

    fix style cover components

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -10,7 +10,7 @@ const Cover = ({hide, setHide}: any) => {
     }
     
     return (
-        <section className={`${style.bgCover} vh-100 text-white`}>
+        <section className={`${style.bgCover} vh-100 text-white d-flex justify-content-center flex-column align-items-center`}>
             <div className={`rounded-5 ${style.wCover} position-relative z-10`}>
                 <div className={`${style.bgBlack50} text-center py-5 rounded-4`}>
                     <img className='rounded-circle py-3' style={{maxHeight: "40vh"}} src={ profileImage } alt="pengantin" />
