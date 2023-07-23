a11c70946e64b83951730022898c9cb4b98b1bab
kiwi-ui <yusnianggara@gmail.com>
2023-07-23 17:00:12 +0700

    final commit

--- src/components/EventDetails/index.tsx
+++ src/components/EventDetails/index.tsx
@@ -40,9 +40,9 @@ const EventDetails = () => {
     return (
         <section id="date" className={ `border-0 d-flex justify-content-center h-100 my-4 ${style.z10}` }>
             <div className={` ${ style.schedule } border-box ${style.bgColorPrimary} h-100 my-5`} style={{backgroundImage: '../../assets/img/cover.jpg', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
-                <div className={` ${style.coverSchedule} d-flex align-items-bottom text-white justify-content-end align-items-center text-center flex-column`} style={{height: '60vh'}}>
-                    <p className={`m-0 text-color-primary fs-1 fw-semibold ${style.z10}`}>Waktu dan Tempat</p>
-                    <p className={`m-0 text-color-primary fs-2 fw-semibold ${style.z10}`}>Pernikahan</p>
+                <div className={` ${style.coverSchedule} d-flex align-items-bottom text-white justify-content-end align-items-center text-center flex-column position-relative`} style={{height: '60vh'}}>
+                    <p className={`m-0 ${style.txtColorPrimary} fs-1 fw-semibold ${style.z10}`}>Waktu dan Tempat</p>
+                    <p className={`m-0 ${style.txtColorPrimary} fs-2 fw-semibold ${style.z10}`}>Pernikahan</p>
                     
                     <div className="mb-5 mt-2 z-10 text-white">
                         <p className="m-0">30 Februari 2001</p>
