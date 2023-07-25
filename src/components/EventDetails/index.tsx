a68b0bfde9659330c7e96141c26997a3aa504f3d
kiwi-ui <yusnianggara@gmail.com>
2023-07-25 14:40:53 +0700

    del bootsttrap

--- src/components/EventDetails/index.tsx
+++ src/components/EventDetails/index.tsx
@@ -40,7 +40,7 @@ const EventDetails = () => {
     return (
         <section id="date" className={ `border-0 d-flex justify-content-center h-100 my-4 ${style.z10}` }>
             <div className={`${style.schedule} border-box ${style.bgColorPrimary} h-100 my-5`}>
-                <div className={` ${style.coverSchedule} d-flex align-items-bottom text-white justify-content-end align-items-center text-center flex-column position-relative`} style={{height: '60vh',backgroundImage: "url('../../assets/img/bride.jpg')"}}>
+                <div className={`  d-flex align-items-bottom text-white justify-content-end align-items-center text-center flex-column position-relative`} style={{height: '60vh',backgroundImage: "url('../../assets/img/bride.jpg')"}}>
                     <p className={`m-0 ${style.txtColorPrimary} fs-1 fw-semibold ${style.z10}`}>Waktu dan Tempat</p>
                     <p className={`m-0 ${style.txtColorPrimary} fs-2 fw-semibold ${style.z10}`}>Pernikahan</p>
                     
