f71be1d8a7079dd2f9efb5a7eed0fc7349a20b60
kiwi-ui <yusnianggara@gmail.com>
2023-07-25 19:44:33 +0700

    responsive

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -11,7 +11,7 @@ const Cover = ({hide, setHide}: any) => {
     }
     
     return (
-        <section className={`${style.bgCover} vh-100 text-white d-flex justify-content-center flex-column align-items-center overflow-hidden`}>
+        <section className={`${style.bgCover} text-white d-flex justify-content-center flex-column align-items-center overflow-hidden`}>
             <div className={`rounded-5 ${style.wCover} position-relative z-10`}>
                 <div className={`${style.bgBlack50} text-center py-5 rounded-4`}>
                     <img className='rounded-circle py-3' style={{maxWidth: "35%"}} src={ profileImage } alt="pengantin" />
