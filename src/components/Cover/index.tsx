77a2710f0056c80d481d97628d042d7dc4cee8a4
kiwi-ui <yusnianggara@gmail.com>
2023-07-25 19:29:11 +0700

    responsive

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -11,17 +11,17 @@ const Cover = ({hide, setHide}: any) => {
     }
     
     return (
-        <section className={`${style.bgCover} vh-100 text-white d-flex justify-content-center flex-column align-items-center`}>
+        <section className={`${style.bgCover} vh-100 text-white d-flex justify-content-center flex-column align-items-center overflow-none`}>
             <div className={`rounded-5 ${style.wCover} position-relative z-10`}>
                 <div className={`${style.bgBlack50} text-center py-5 rounded-4`}>
-                    <img className='rounded-circle py-3' style={{maxWidth: "40%"}} src={ profileImage } alt="pengantin" />
-                    <p className='m-0'>UNDANGAN PERNIKAHAN</p>
+                    <img className='rounded-circle py-3' style={{maxWidth: "35%"}} src={ profileImage } alt="pengantin" />
+                    <h5 className='m-0'>UNDANGAN PERNIKAHAN</h5>
                     
                     <div className='d-flex flex-column gap-2'>
-                        <h2 className={`fw-bold ${style.textColorPrimary}`}>James & Humairoh</h2>
-                        <p className="mb-0">Kepada yang terhormat</p>
-                        <p className="mb-1">Bapak/Ibu/Saudara</p>
-                        <h4 className={`mt-2 fw-bold mb-4 ${style.textColorPrimary}`}>{ name }</h4>
+                        <h1 className={`fw-bold ${style.textColorPrimary} m-2   `}>James & Humairoh</h1>
+                        <h5 className="m-0">Kepada yang terhormat</h5>
+                        <h5 className="m-0">Bapak/Ibu/Saudara</h5>
+                        <h1 className={`mt-2 fw-bold mb-4 ${style.textColorPrimary}`}>{ name }</h1>
                     </div>
 
                     <button onClick={ hideCover } className={ `${style.bgButton} rounded-4 px-3 py-2 position-relative start-50 translate-middle-x text-white border-0 d-flex flex-row align-items-center justify-content-center gap-2`}>
