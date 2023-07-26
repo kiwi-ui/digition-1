59103090a42a8aba40e93821cc155284c0989b7f
kiwi-ui <yusnianggara@gmail.com>
2023-07-26 10:20:42 +0700

    cover

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -11,22 +11,22 @@ const Cover = ({hide, setHide}: any) => {
     }
     
     return (
-        <section className={`${style.bgCover} text-white d-flex justify-content-center flex-column align-items-center overflow-hidden`}>
-            <div className={`rounded-5 ${style.wCover} position-relative z-10`}>
+        <section className={`${ style.bgCover } text-white d-flex justify-content-center flex-column align-items-center overflow-hidden`}>
+            <div className={`rounded-5 ${ style.wCover } position-relative z-10`}>
                 <div className={`${style.bgBlack50} text-center py-5 rounded-4`}>
-                    <img className='rounded-circle py-3' style={{maxWidth: "35%"}} src={ profileImage } alt="pengantin" />
-                    <h5 className='m-0'>UNDANGAN PERNIKAHAN</h5>
+                    <img className='rounded-circle py-3' style={{ maxWidth: "45%" }} src={ profileImage } alt="pengantin" />
+                    <p className='m-0 fs-1'>UNDANGAN PERNIKAHAN</p>
                     
                     <div className='d-flex flex-column gap-2'>
-                        <h1 className={`fw-bold ${style.textColorPrimary} m-2`}>Yusni & Anggara</h1>
-                        <h5 className="m-0">Kepada yang terhormat</h5>
-                        <h5 className="m-0">Bapak/Ibu/Saudara</h5>
-                        <h1 className={`mt-2 fw-bold mb-4 ${style.textColorPrimary}`}>{ name }</h1>
+                        <p className={`fw-bold ${style.textColorPrimary} m-2`} style={{fontSize: '5rem'}}>Yusni & Anggara</p>
+                        <p className="fs-1 m-0">Kepada yang terhormat</p>
+                        <p className="fs-1 m-0">Bapak/Ibu/Saudara</p>
+                        <p className={`mt-2 fw-bold mb-4 ${style.textColorPrimary}`} style={{ fontSize: "5rem" }}>{ name }</p>
                     </div>
 
-                    <button onClick={ hideCover } className={ `${style.bgButton} rounded-4 px-3 py-2 position-relative start-50 translate-middle-x text-white border-0 d-flex flex-row align-items-center justify-content-center gap-2`}>
-                        <BsEnvelopeHeart/>
-                        <span className="text-white text-decoration-none fw-bold">Buka Undangan</span>
+                    <button onClick={ hideCover } className={ `${style.bgButton} rounded-4 px-4 py-3 position-relative start-50 translate-middle-x text-white border-0 d-flex flex-row align-items-center justify-content-center gap-3`}>
+                        <BsEnvelopeHeart className="fs-1"/>
+                        <span className="text-white text-decoration-none fw-bold fs-1">Buka Undangan</span>
                     </button>
                 </div>
             </div>
