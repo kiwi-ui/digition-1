27ca3bd8d45eef418542499b936414c9b31c0c78
kiwi-ui <yusnianggara@gmail.com>
2023-07-26 10:35:22 +0700

    headline change font size

--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -4,9 +4,9 @@ const Headline = () => {
     return (
         <section id="headline" className={`d-block ${style.bgHeadline} vh-100 d-flex align-items-center justify-content-center`}>
             <div className="d-flex flex-column justify-content-center align-items-center">
-                <h1 className={`fw-bold m-0`} style={{ color: '#e3b04b'}}>The Wedding Of</h1>
-                <p className={ `fs-1 fw-bold py-2 px-3 m-0` } style={{ color: '#e3b04b' ,letterSpacing: 2 }}>James & Humairoh</p>
-                <h3 className="text-white m-0">30-02-2090</h3>
+                <h1 className={`fw-bold m-0`} style={{ color: '#e3b04b', fontSize: "3.5rem"}}>The Wedding Of</h1>
+                <p className={ `fw-bold py-2 px-3 m-1` } style={{ color: '#e3b04b' ,letterSpacing: 2, fontSize: '4.5rem' }}>James & Humairoh</p>
+                <p className="text-white m-0" style={{ fontSize: "3.5rem" }}>30-02-2090</p>
             </div>
         </section>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import './index.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
